b3cd340780b278a41b83e523bc3a8cd552a7f370
aryan1222 <chhabraaryan12@gmail.com>
2021-03-09 00:34:33 +0530

    added breadcrumbs

--- confusion/src/components/DishDetail.js
+++ confusion/src/components/DishDetail.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Card, CardBody, CardImg, CardText, CardTitle} from 'reactstrap'
+import { Card, CardBody, CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap'
+import {Link} from 'react-router-dom'
     
 function RenderComments(props){
         if(props.comments!=null){
@@ -31,6 +32,20 @@ function RenderComments(props){
 export const DishDetail = (props) => {
     return (
         <div className="container">
+
+            <div className='row'>
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                
+                <div className='col-12'>
+                    <h3>{props.dish.name}</h3>
+                    <hr/>
+                </div>  
+            </div>
+
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
                     <Card>
