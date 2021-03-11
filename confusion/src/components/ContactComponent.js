e0fb8db9a1ea5eafb79b98974f231d8422323431
aryan1222 <chhabraaryan12@gmail.com>
2021-03-12 00:19:51 +0530

    feedback form added

--- confusion/src/components/ContactComponent.js
+++ confusion/src/components/ContactComponent.js
@@ -1,51 +1,140 @@
-import React from 'react'
-import {Breadcrumb, BreadcrumbItem} from 'reactstrap'
+import React, {Component} from 'react'
+import {Breadcrumb, BreadcrumbItem, FormGroup, Form, Label, Input, Button} from 'reactstrap'
 import {Link} from 'react-router-dom'
 
-const ContactComponent = () => {
-    return (
-        <div className="container">
+class ContactComponent extends Component {
 
-            <div className='row'>
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>Contact Us</BreadcrumbItem>
-                </Breadcrumb>
-                
-                <div className='col-12'>
-                    <h3>Contact Us</h3>
-                    <hr/>
-                </div>  
-            </div>
-
-            <div className="row row-content">
-                <div className="col-12">
-                <h3>Location Information</h3>
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             firstname:'',
+             lastname:'',
+             telnum:'',
+             email:'',
+             agree:false,
+             contactType:'Tel.',
+             message:''
+        }
+    }
+    
+    render() {
+        return (
+            <div className="container">
+    
+                <div className='row'>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                    </Breadcrumb>
+                    
+                    <div className='col-12'>
+                        <h3>Contact Us</h3>
+                        <hr/>
+                    </div>  
                 </div>
-                <div className="col-12 col-sm-4 offset-sm-1">
-                        <h5>Our Address</h5>
-                        <address>
-                        121, Clear Water Bay Road<br />
-                        Clear Water Bay, Kowloon<br />
-                        HONG KONG<br />
-                        <i className="fa fa-phone"></i>: +852 1234 5678<br />
-                        <i className="fa fa-fax"></i>: +852 8765 4321<br />
-                        <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
-                        </address>
-                </div>
-                <div className="col-12 col-sm-6 offset-sm-1">
-                    <h5>Map of our Location</h5>
-                </div>
-                <div className="col-12 col-sm-11 offset-sm-1">
-                    <div className="btn-group" role="group">
-                        <a role="button" className="b+tn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                        <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
-                        <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+    
+                <div className="row row-content">
+                    <div className="col-12">
+                    <h3>Location Information</h3>
+                    </div>
+                    <div className="col-12 col-sm-4 offset-sm-1">
+                            <h5>Our Address</h5>
+                            <address>
+                            121, Clear Water Bay Road<br />
+                            Clear Water Bay, Kowloon<br />
+                            HONG KONG<br />
+                            <i className="fa fa-phone"></i>: +852 1234 5678<br />
+                            <i className="fa fa-fax"></i>: +852 8765 4321<br />
+                            <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                            </address>
+                    </div>
+                    <div className="col-12 col-sm-6 offset-sm-1">
+                        <h5>Map of our Location</h5>
+                    </div>
+                    <div className="col-12 col-sm-11 offset-sm-1">
+                        <div className="btn-group" role="group">
+                            <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                            <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
+                            <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                        </div>
                     </div>
                 </div>
+    
+                <div className="row row-content">
+
+                    <div className="col-12 my-3">
+                        <h1>Send us Your Feedback</h1>
+                    </div>
+    
+                    <div className="col-12 col-md-9">
+                        <Form>
+                            <FormGroup row>
+                                <Label htmlFor="firstname" sm={12} md={2} xl={2} lg={2} >First Name</Label>
+                                <div className="col" sm={12} md={10} xl={10} lg={10}>
+                                    <Input type="text" id="firstname" placeholder="First Name" value={this.state.firstname}></Input>
+                                </div>
+                            </FormGroup> 
+    
+                            <FormGroup row>
+                                <Label htmlFor="lastname" sm={12} md={2} xl={2} lg={2} >Last Name</Label>
+                                <div className="col" sm={12} md={10} xl={10} lg={10}>
+                                    <Input type="text" id="lastname" placeholder="Last Name" value={this.state.lastname}></Input>
+                                </div>
+                            </FormGroup>
+
+                            <FormGroup row>
+                                <Label htmlFor="telnum" sm={12} md={2} xl={2} lg={2} >Contact Tel.</Label>
+                                <div className="col" sm={12} md={10} xl={10} lg={10}>
+                                    <Input type="tel" id="telnum" placeholder="Tel. Number" value={this.state.telnum}></Input>
+                                </div>
+                            </FormGroup> 
+
+                            <FormGroup row>
+                                <Label htmlFor="email" sm={12} md={2} xl={2} lg={2} >Email</Label>
+                                <div className="col" sm={12} md={10} xl={10} lg={10}>
+                                    <Input type="email" id="email" placeholder="Email" value={this.state.email}></Input>
+                                </div>
+                            </FormGroup>
+
+                            <FormGroup row>
+                                <div className="col" md={{size: 6, offset:3}}>
+                                    <FormGroup check>
+                                        <Label check>
+                                            <Input type="checkbox" name="agree" checked={this.state.agree}/>
+                                            {' '}<strong>May we contact you ?</strong>
+                                        </Label>
+                                    </FormGroup>
+                                </div>
+                                <div className="col" md={{size: 3, offset:1}}>
+                                    <Input type="select" name="contactType" value={this.state.contactType}>
+                                        <option>Tel.</option>
+                                        <option>Email</option>
+                                    </Input>
+                                </div>
+                            </FormGroup>
+
+                            <FormGroup row>
+                                <Label htmlFor="message" sm={12} md={2} xl={2} lg={2} >Your Feedback</Label>
+                                <div className="col" sm={12} md={10} xl={10} lg={10}>
+                                    <Input rows="12" type="textarea" id="message" value={this.state.message}></Input>
+                                </div>
+                            </FormGroup>
+
+                            <FormGroup row>
+                                <div className="col" md={{size:10, offset: 2}}>
+                                    <Button type="submit" color="primary">Send Feedback</Button>
+                                </div>
+                            </FormGroup>
+                        </Form>
+                    </div>
+
+                </div>
+
             </div>
-        </div>
-    );
+        );
+    
+    }
 }
 
 export default ContactComponent;
