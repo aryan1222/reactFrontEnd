0610d4553c0b6e5ba0ca9e6307a09a616b3c3250
aryan1222 <chhabraaryan12@gmail.com>
2021-03-13 14:32:00 +0530

    sync form inputs and state

--- confusion/src/components/ContactComponent.js
+++ confusion/src/components/ContactComponent.js
@@ -5,19 +5,40 @@ import {Link} from 'react-router-dom'
 class ContactComponent extends Component {
 
     constructor(props) {
-        super(props)
-    
+        super(props);
+
         this.state = {
-             firstname:'',
-             lastname:'',
-             telnum:'',
-             email:'',
-             agree:false,
-             contactType:'Tel.',
-             message:''
-        }
+            firstname: '',
+            lastname: '',
+            telnum: '',
+            email: '',
+            agree: false,
+            contactType: 'Tel.',
+            message: ''
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        
     }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
     
+        this.setState({
+          [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        console.log('Current State is: ' + JSON.stringify(this.state));
+        alert('Current State is: ' + JSON.stringify(this.state));
+        event.preventDefault();
+        // prevent opening the next page
+    }
+
     render() {
         return (
             <div className="container">
@@ -68,32 +89,32 @@ class ContactComponent extends Component {
                     </div>
     
                     <div className="col-12 col-md-9">
-                        <Form>
+                        <Form onSubmit={this.handleSubmit}>
                             <FormGroup row>
                                 <Label htmlFor="firstname" sm={12} md={2} xl={2} lg={2} >First Name</Label>
                                 <div className="col" sm={12} md={10} xl={10} lg={10}>
-                                    <Input type="text" id="firstname" placeholder="First Name" value={this.state.firstname}></Input>
+                                    <Input type="text" id="firstname" placeholder="First Name" name="firstname" value={this.state.firstname} onChange={this.handleInputChange} />
                                 </div>
                             </FormGroup> 
     
                             <FormGroup row>
                                 <Label htmlFor="lastname" sm={12} md={2} xl={2} lg={2} >Last Name</Label>
                                 <div className="col" sm={12} md={10} xl={10} lg={10}>
-                                    <Input type="text" id="lastname" placeholder="Last Name" value={this.state.lastname}></Input>
+                                    <Input type="text" id="lastname" placeholder="Last Name" name="lastname" value={this.state.lastname} onChange={this.handleInputChange}/>
                                 </div>
                             </FormGroup>
 
                             <FormGroup row>
                                 <Label htmlFor="telnum" sm={12} md={2} xl={2} lg={2} >Contact Tel.</Label>
                                 <div className="col" sm={12} md={10} xl={10} lg={10}>
-                                    <Input type="tel" id="telnum" placeholder="Tel. Number" value={this.state.telnum}></Input>
+                                    <Input type="tel" id="telnum" placeholder="Tel. Number" name="telnum" value={this.state.telnum} onChange={this.handleInputChange}></Input>
                                 </div>
                             </FormGroup> 
 
                             <FormGroup row>
                                 <Label htmlFor="email" sm={12} md={2} xl={2} lg={2} >Email</Label>
                                 <div className="col" sm={12} md={10} xl={10} lg={10}>
-                                    <Input type="email" id="email" placeholder="Email" value={this.state.email}></Input>
+                                    <Input type="email" id="email" placeholder="Email" name="email" value={this.state.email} onChange={this.handleInputChange}></Input>
                                 </div>
                             </FormGroup>
 
@@ -101,13 +122,13 @@ class ContactComponent extends Component {
                                 <div className="col" md={{size: 6, offset:3}}>
                                     <FormGroup check>
                                         <Label check>
-                                            <Input type="checkbox" name="agree" checked={this.state.agree}/>
+                                            <Input type="checkbox" name="agree" checked={this.state.agree} onChange={this.handleInputChange}/>
                                             {' '}<strong>May we contact you ?</strong>
                                         </Label>
                                     </FormGroup>
                                 </div>
                                 <div className="col" md={{size: 3, offset:1}}>
-                                    <Input type="select" name="contactType" value={this.state.contactType}>
+                                    <Input type="select" name="contactType" value={this.state.contactType} onChange={this.handleInputChange}>
                                         <option>Tel.</option>
                                         <option>Email</option>
                                     </Input>
@@ -117,7 +138,7 @@ class ContactComponent extends Component {
                             <FormGroup row>
                                 <Label htmlFor="message" sm={12} md={2} xl={2} lg={2} >Your Feedback</Label>
                                 <div className="col" sm={12} md={10} xl={10} lg={10}>
-                                    <Input rows="12" type="textarea" id="message" value={this.state.message}></Input>
+                                    <Input rows="12" type="textarea" id="message" name="message" value={this.state.message} onChange={this.handleInputChange}></Input>
                                 </div>
                             </FormGroup>
 
