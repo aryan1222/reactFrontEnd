b3829c8713ea2d495fcfe0c9cbd0f1da12eefcfb
aryan1222 <chhabraaryan12@gmail.com>
2021-03-26 15:19:36 +0530

    splitting and combining reducers

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,11 +1,17 @@
-import { createStore } from 'redux'
-import {initialState, reducer} from './reducer'
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Leaders } from './leaders';
+import { Promotions } from './promotions';
+
+const rootStore = combineReducers({
+    dishes : Dishes,
+    comments : Comments,
+    promotions : Promotions,
+    leaders : Leaders
+});
 
 export const configureStore = () => {
-    const store = createStore(
-        reducer,
-        initialState
-    );
-
+    const store = createStore(rootStore);
     return store;
 }
